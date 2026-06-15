b0c50ef3691491c9a81731c9f267eba8ce1ab09b
mkekn23 <mkekn23@freeuni.edu.ge>
2026-06-15 14:10:24 +0400

    feat: setup project infrastructure with spring boot, react vite and docker mysql

---
+++ backend/src/test/java/com/serialtracker/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serialtracker.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
